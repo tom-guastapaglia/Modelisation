6f95360d920fe9b86f07bb965e90dc0ebf83c7c0
Samy-Pelisson <samypelissonyt@gmail.com>
2020-09-25 09:39:11 +0200

    w accessible

--- rename.py
+++ rename.py
@@ -4,6 +4,8 @@ master = Tk()
 
 MatSizeX = 11
 MatSizeY = 11
+canWidth = 60
+canHeight = 60
 
 
 def initWindow(canWidth, canHeight):
@@ -11,6 +13,12 @@ def initWindow(canWidth, canHeight):
     heightW = canHeight * MatSizeY + MatSizeY
     w = Canvas(master, width=widthW, height=heightW)
     w.pack()
+    return w
+
+
+def refreshGrille(w,canWidth, canHeight):
+    widthW = canWidth * MatSizeX + MatSizeX
+    heightW = canHeight * MatSizeY + MatSizeY
 
     for i in range(0, MatSizeY):
         begX = 0
@@ -24,5 +32,8 @@ def initWindow(canWidth, canHeight):
         w.create_line(X, begY, X, endY, fill="#476042")
 
 
-initWindow(60, 60)
+w = initWindow(canWidth,canHeight)
+refreshGrille(w, canWidth, canHeight)
+#w.create_line(50, 50, 100, 50, fill="#476042")
+
 mainloop()
