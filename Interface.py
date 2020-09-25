d881dec2f24d374e640d9b17eb45d4b84a53a1be
Tom-Guastapaglia <tomguasta@yahoo.fr>
2020-09-25 10:04:26 +0200

    Premiere iteration

--- Interface.py
+++ Interface.py
@@ -33,8 +33,25 @@ def refreshGrille(w,canWidth, canHeight):
         w.create_line(X, begY, X, endY, fill="#476042")
 
 
+
+def color_case(t, w, canHeight):
+    pas = canHeight+1
+    rows, cols = t.shape
+    for i in range (0,rows):
+        for j in range(0, cols):
+            if (t[i,j]==1):
+                w.create_rectangle(pas*i,pas*j,pas*i+pas,pas*j+pas,fill='black')
+
+
+
+
 w = initWindow(canWidth,canHeight)
 refreshGrille(w, canWidth, canHeight)
+color_case(t, w, canHeight)
+for i in range(0,12):
+    refreshGrille(w, canWidth, canHeight)
+    color_case(t, w, canHeight)
+    iterations(t)
 #w.create_line(50, 50, 100, 50, fill="#476042")
 
 mainloop()
--- jeuDeLaVie.py
+++ jeuDeLaVie.py
@@ -1,5 +1,5 @@
-import numpy as np;
-import time;
+import numpy as np
+import time
 import os
 
 
